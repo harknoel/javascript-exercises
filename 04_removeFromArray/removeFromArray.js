0352e8b159dde70847d2ee663baee36453ba9b2c
harknoel <bontilao.harknoel@gmail.com>
2023-06-12 03:37:53 +0800

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+    let arr = array;
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(nums[i] == arr[j] && nums[i] === arr[j]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
