1e4d259f659a8ffb823c45a2209c544d944861d3
harknoel <bontilao.harknoel@gmail.com>
2023-06-12 03:48:50 +0800

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if((start < 0 || end < 0) || (!Number.isInteger(start) || !Number.isInteger(end))) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if(start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
