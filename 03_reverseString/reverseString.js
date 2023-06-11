88dfde25c33ab6ed0e9c286d7e040a1fc7bd6313
harknoel <bontilao.harknoel@gmail.com>
2023-06-12 01:15:35 +0800

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let words = "";
+    for(let i = word.length - 1; i >= 0; i--) {
+        words += word[i];
+    }
+    return words;
 };
 
 // Do not edit below this line
