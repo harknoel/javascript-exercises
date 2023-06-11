a72398cd886201ec3e53d89e45016d95b05878e5
harknoel <bontilao.harknoel@gmail.com>
2023-06-12 04:18:26 +0800

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  if(Number.isInteger(celsius)) {
+    return celsius;
+  } 
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  if(Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  } 
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
